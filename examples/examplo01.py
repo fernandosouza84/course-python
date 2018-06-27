d12a4719f82af892199cd9ab976a11e0cdbd86e5
Fernando Souza <fernando.de.souza@gmail.com>
2018-06-27 13:32:48 -0300

    first example

---
+++ examples/examplo01.py
@@ -0,0 +1,13 @@
+#example with string
+nome = 'Fernando'
+sobreNome = 'Souza'
+iniciais = nome[0] + sobreNome[0]
+
+print(iniciais)
+print('\n')
+
+#example with numbers
+numUm = 1;
+numDois = 1.5;
+print(type(numUm))
+print(type(numDois))
